2d3e9da494df91cbd043f63c81107176071522bd
Holyvert <96556635+Holyvert@users.noreply.github.com>
2024-04-12 17:44:55 -0400

    Revert "trying to connect to firebase"
    This reverts commit caa7bfb35f29d36bca69fae88aa6a4c606af67a4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { AngularFireModule } from '@angular/fire/compat';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideDatabase, getDatabase } from '@angular/fire/database';
-import { provideStorage, getStorage } from '@angular/fire/storage';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './pages/landing/landing.component';
 import { HeaderComponent } from './components/header/header.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { firebaseConfig } from './environment/environment';
 
 @NgModule({
   declarations: [AppComponent, LandingComponent, HeaderComponent],
@@ -31,15 +22,6 @@ import { firebaseConfig } from './environment/environment';
     MatIconModule,
     MatButtonModule,
     MatExpansionModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
-    provideStorage(() => getStorage()),
-    AngularFireAuthModule,
-    AngularFireStorageModule,
-    AngularFirestoreModule,
-    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
